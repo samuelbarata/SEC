346fa1ead9e1b482c1559bdd544bf646ee3e01b3
Pedro Godinho <pedro.fernandez.godinho@gmail.com>
2022-04-21 16:37:43 +0100

    Add base client

---
+++ client/src/main/java/pt/ulisboa/tecnico/sec/candeeiros/client/InteractiveClient.java
@@ -0,0 +1,132 @@
+package pt.ulisboa.tecnico.sec.candeeiros.client;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import pt.ulisboa.tecnico.sec.candeeiros.Bank;
+import pt.ulisboa.tecnico.sec.candeeiros.client.exceptions.FailedChallengeException;
+import pt.ulisboa.tecnico.sec.candeeiros.client.exceptions.WrongNonceException;
+import pt.ulisboa.tecnico.sec.candeeiros.shared.Crypto;
+import pt.ulisboa.tecnico.sec.candeeiros.shared.Nonce;
+
+import java.io.IOException;
+import java.security.*;
+import java.security.spec.InvalidKeySpecException;
+import java.util.Scanner;
+
+public class InteractiveClient {
+    private final Logger logger = LoggerFactory.getLogger(InteractiveClient.class);
+    private final PublicKey publicKey;
+    private final PrivateKey privateKey;
+    private final String target;
+    private final BankClient client;
+    private Nonce nonce = null;
+
+    public InteractiveClient(String target, String privateKeyFile, String publicKeyFile) {
+        this.target = target;
+        this.publicKey = (PublicKey) Crypto.readKeyOrExit(publicKeyFile, "pub");
+        this.privateKey = (PrivateKey) Crypto.readKeyOrExit(privateKeyFile, "private");
+        client = new BankClient(target);
+    }
+
+    private void negotiateNonce() throws FailedChallengeException {
+        if (nonce != null)
+            return;
+        Bank.NonceNegotiationResponse response = client.nonceNegotiation(publicKey);
+        nonce = Nonce.decode(response.getNonce());
+    }
+
+    private Bank.OpenAccountResponse openAccount() throws FailedChallengeException {
+        Bank.OpenAccountResponse response = client.openAccount(publicKey);
+        negotiateNonce();
+        return response;
+    }
+
+    private Bank.SendAmountResponse sendAmount(PublicKey destination, String amount) throws WrongNonceException, FailedChallengeException {
+        negotiateNonce();
+        Bank.SendAmountResponse response = client.sendAmount(publicKey, destination, amount, nonce);
+        nonce = Nonce.decode(response.getNonce());
+        return response;
+    }
+
+    private Bank.CheckAccountResponse checkAccount(PublicKey publicKey) throws FailedChallengeException {
+        Bank.CheckAccountResponse response = client.checkAccount(publicKey);
+        return response;
+    }
+
+    public void run() {
+        String line;
+        Nonce nonce = null;
+
+        while (true) {
+            Scanner scan = new Scanner(System.in);
+
+            System.out.print("Enter a command: ");
+            line = scan.nextLine();
+            String[] commandArgs = line.split(" ");
+            switch (commandArgs[0]) {
+                case "exit":
+                    client.shutdown();
+                    return;
+                case "open_account":
+                    try {
+                        openAccount();
+                    } catch (FailedChallengeException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                case "send_amount":
+                    if (commandArgs.length != 3) {
+                        System.out.println("Not enough arguments");
+                    } else {
+                        try {
+                            PublicKey destination = (PublicKey) Crypto.readKey(commandArgs[1], "pub");
+                            sendAmount(destination, commandArgs[2]);
+                        } catch (WrongNonceException | FailedChallengeException e) {
+                            e.printStackTrace();
+                        } catch (IOException | NoSuchAlgorithmException | InvalidKeySpecException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    break;
+                case "check_account":
+                    if (commandArgs.length != 2) {
+                        System.out.println("Not enough arguments");
+                    } else {
+                        try {
+                            PublicKey account = (PublicKey) Crypto.readKey(commandArgs[1], "pub");
+                            checkAccount(account);
+                        } catch (IOException | NoSuchAlgorithmException | InvalidKeySpecException | FailedChallengeException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    break;
+                default:
+                    System.out.println("Unknown Command");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        // check arguments
+        if (args.length < 4) {
+            System.err.println("Argument(s) missing!");
+            return;
+        }
+
+        final String host = args[0];
+        final int port = Integer.parseInt(args[1]);
+        final String target = host + ":" + port;
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter private key file location: ");
+        String privateKeyFile = scan.nextLine();
+        System.out.println("Enter public key file location: ");
+        String publicKeyFile = scan.nextLine();
+
+        InteractiveClient client = new InteractiveClient(target, privateKeyFile, publicKeyFile);
+        client.run();
+    }
+}
+
